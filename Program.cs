51a1283752f4025888bd47dd0d4ee67c71477b4f
Caroline Brownlee <brownleecaroline@gmail.com>
2020-04-27 23:55:55 -0500

    set up initial files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Custom_Types_with_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
